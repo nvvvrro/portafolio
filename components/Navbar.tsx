4a445bd66b1fa363131e2abd53c0301a2a106327
nvvvrro <m.navarrocarrillo@gmail.com>
2021-11-07 12:45:59 -0300

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { NextComponentType } from "next";
+
+const Navbar: NextComponentType = () => {
+  return <div>Hello Wold</div>;
+};
+
+export default Navbar;
