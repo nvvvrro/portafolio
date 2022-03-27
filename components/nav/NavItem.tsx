84f406c78de59a6b97c1871a8ff4aa9d93d567c4
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-03-26 23:56:16 -0300

    fix: NavItem

--- components/nav/NavItem.tsx
+++ components/nav/NavItem.tsx
@@ -10,18 +10,16 @@ export const NavItem: FC<Props> = ({ title, href }) => {
   const hrefLowerCase = title.toLowerCase();
 
   return (
-    <ul className="text-secondary-main">
-      <Link href={href ? href : `/${hrefLowerCase}`}>
-        <a className="hover: transition-colors ">
-          <motion.h2
-            initial={{ opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.5 }}
-          >
-            {title}
-          </motion.h2>
-        </a>
-      </Link>
-    </ul>
+    <Link href={href ? href : `/${hrefLowerCase}`}>
+      <a className="hover: transition-colors">
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          {title}
+        </motion.h2>
+      </a>
+    </Link>
   );
 };
