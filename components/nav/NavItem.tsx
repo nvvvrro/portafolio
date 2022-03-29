a763d8ae8ae77e3c771a2ccc629ff301e792cfdc
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-03-29 20:08:12 -0300

    fix: motion in item nav

--- components/nav/NavItem.tsx
+++ components/nav/NavItem.tsx
@@ -10,12 +10,13 @@ export const NavItem: FC<Props> = ({ title, href }) => {
   const hrefLowerCase = title.toLowerCase();
 
   return (
-    <Link href={href ? href : `/${hrefLowerCase}`}>
-      <a className="hover: transition-colors">
+    <Link href={href ? href : `/${hrefLowerCase}`} passHref>
+      <a className="hover:transition-colors hover:no-underline hover:opacity-80">
         <motion.h2
           initial={{ opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 0.5 }}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9 }}
         >
           {title}
         </motion.h2>
