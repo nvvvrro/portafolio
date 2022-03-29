d93929b690c2b38291f392fb4049b53ef37aa599
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-03-28 23:04:14 -0300

    fix: dependencies index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { siteTitle, Layout, Navbar } from "components";
+import { siteTitle, Layout } from "components";
 import { getSortedPostsData } from "../lib/posts";
+import { PostCard, Categories, PostWidget } from "components/post";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -12,14 +13,14 @@ export async function getStaticProps() {
   };
 }
 
-interface NextPageProps {
+interface PostProps {
   allPostsData: {
     date: string;
     title: string;
     id: string;
   }[];
 }
-const Home: NextPage<NextPageProps> = ({ allPostsData }) => {
+const Home: NextPage<PostProps> = ({ allPostsData }) => {
   return (
     <Layout>
       <Head>
@@ -27,25 +28,36 @@ const Home: NextPage<NextPageProps> = ({ allPostsData }) => {
       </Head>
       <section>
         <p>
-          Hola, soy Marcelo, Un placer conocerte, Bienvenido a mi blog. Aquí
-          podrás encontrar projectos, trabajos y artículos sobre el desarrollo
-          web con distintas tecnologías. Espero que te encuentres en el lugar
-          correcto.
+          Hola a todos, soy Marcelo Navarro, un desarrollador de software,
+          titulado como Ingeniero en Informática.
+          <br />
+          <br />
+          Me gusta aprender y compartir conocimientos, es por eso que existe
+          este blog, para que puedas conocer más sobre mí, como mis intereses,
+          ideas, soluciones, experiencia profesional y mi forma de trabajar.
+          <br />
+          <br />
+          Te invito a que leas mis artículos y te pongas en contacto conmigo si
+          tienes alguna duda o sugerencia.
+          <br />
+          <br />
         </p>
       </section>
-      <section >
-        <h2 >Blog</h2>
-        <ul >
-          {allPostsData.map(({ id, date, title }) => (
-            <li  key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
+      <section>
+        <h2>Blog</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="lg:col-span-8 col-span-1">
+            {allPostsData.map((post, id) => (
+              <PostCard post={post} key={id} />
+            ))}
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
+          </div>
+        </div>
       </section>
     </Layout>
   );
