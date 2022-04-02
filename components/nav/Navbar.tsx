cbd2b5830208edf2370c6d66158da78c52e14fbd
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-04-01 23:57:02 -0300

    fix: dimension navbar

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
   <nav className="container sticky mx-auto top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 dark:bg-opacity-10 border-b border-gray-200 dark:border-gray-600 shadow-sm min-w-full ">
       <div className="flex flex-col max-w-7xl mx-auto gap-2">
-        <div className="flex relative items-center justify-between h-16">
+        <div className="flex relative items-center justify-between h-16 px-8">
           <div className="flex items-center">
             <Image
               className="2xl:block cursor-pointer hover:opacity-80 transition-opacity duration-200 h-full w-full block mx-auto"
@@ -16,7 +16,7 @@ export const Navbar = () => {
             />
           </div>
 
-          <div className="flex-auto w-full py-6 pl-9 gap-4 items-center justify-end px-12 md:px-16">
+          <div className="flex-auto w-full py-6 pl-9 gap-4 items-center justify-end px-8 md:px-12">
             <ul className="flex sm:space-x-14 space-x-8  items-center justify-end font-medium md:text-lg text-sm  text-pink-700 uppercase">
               <NavItem title="Home" href="/" />
               <NavItem title="Blog" />
