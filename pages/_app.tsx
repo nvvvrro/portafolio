19bab2541b7ffcef8a67e893bab04efb0e7f5ee1
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-04-08 23:54:45 -0400

    feat: added animatePresence in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,35 @@
-import "../styles/global.css";
-import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "next-themes";
+import { AnimatePresence } from "framer-motion";
+import type { AppProps } from "next/app";
+import "../styles/global.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  const [isFirstMount, setIsFirstMount] = useState(true);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsFirstMount(false);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <Component
+          isFirstMount={isFirstMount}
+          key={router.route}
+          {...pageProps}
+        />
+      </AnimatePresence>
     </ThemeProvider>
   );
 }
