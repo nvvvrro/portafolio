4594f2e984eed30d7bb41aa1d094406fce287dd3
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-05-04 23:43:14 -0400

    fix: logo animation

--- components/animations/logo/logo.tsx
+++ components/animations/logo/logo.tsx
@@ -1,43 +1,45 @@
-import { FC, useState, useRef, useEffect } from "react";
+import { FC } from "react";
 import { motion } from "framer-motion";
 import style from "./Logo.module.css";
-import clsx from "clsx";
 
-const Container = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1, transition: { duration: 8 } },
+const ContainerLogo = {
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 8,
+      when: "afterChildren",
+    },
+  },
 };
 
 export const Logo: FC = () => {
-
   const activeAnimation = {
     animationStart: () => {
-      document.getElementById("div_logo")?.classList.add(style.div_on);
+      document.getElementById("container_logo")?.classList.add(style.div_on);
     },
     animationComplete: () => {
-      document.getElementById("div_logo")?.classList.remove(style.div_on);
+      document.getElementById("container_logo")?.classList.remove(style.div_on);
     },
   };
 
-  const divStyle = clsx({
-    "text-5xl tracking-widest font-bold border-4 border-solid, p-5 relative h-16 cursor-pointer text-[#e685a4]":
-      true,
-    [style.div_animate]: true,
-  });
-
   return (
     <motion.div
-      id="div_logo"
-      variants={Container}
-      initial="initial"
+      id="container_logo"
+      variants={ContainerLogo}
       animate="animate"
       onAnimationStart={activeAnimation.animationStart}
       onAnimationComplete={activeAnimation.animationComplete}
-      className={divStyle}
+      className={style.div_animated}
     >
-      <span className={`${style.span} flex items-center justify-center`}>
-        nvvvrro
-      </span>
+      <div id="div_logo" className="p-6 relative h-16 cursor-pointer">
+        <span
+          className={
+            "flex items-center justify-center font-bold text-5xl tracking-widest p-5 -translate-y-11"
+          }
+        >
+          nvvvrro
+        </span>
+      </div>
     </motion.div>
   );
 };
