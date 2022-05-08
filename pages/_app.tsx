a29c2d26d0617e44b94249b57617675aa415092e
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-05-07 23:54:39 -0400

    fix: index first presentation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import "../styles/global.css";
 
-
-
 function MyApp({ Component, pageProps, router }: AppProps) {
   const [isFirstMount, setIsFirstMount] = useState(true);
 
