cfa3102a12d94f9acba021cdcf7da950896e5fcd
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-05-10 23:51:53 -0400

    fix: navbar

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,21 +1,12 @@
-import Image from "next/image";
 import { NavItem, DarkMode, Logo } from "components";
 
 export const Navbar = () => {
   return (
     <header className="container sticky mx-auto top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 dark:bg-opacity-10 border-b border-gray-200 dark:border-gray-600 shadow-sm min-w-full ">
-     
       <nav className="flex flex-col max-w-7xl mx-auto gap-2">
         <div className="flex relative items-center justify-between h-20 px-8">
           <div className="flex items-center">
-            {/* <Image
-              className="2xl:block cursor-pointer hover:opacity-80 transition-opacity duration-200 h-full w-full block mx-auto"
-              src="/images/nvvvrro.png"
-              width={340}
-              height={34}
-              alt="Navarro"
-            /> */}
-             <Logo />
+            <Logo />
           </div>
 
           <div className="flex-auto w-full py-6 pl-9 gap-4 items-center justify-end px-8 md:px-16">
