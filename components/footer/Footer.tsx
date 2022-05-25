2d2abb41e6a97d17e396ece7b20341b84d91e7ff
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-05-24 23:55:00 -0400

    fix: footer color

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
-import { user, socialLinks } from "config";
 import { Link } from "components";
+import { user, socialLinks } from "config";
+
 export const Footer: FC = () => {
   return (
-    <footer className="text-gray-700 dark:text-gray-400">
-      <div className="bg-[#F1F1F1] dark:bg-gray-900">
+    <footer className="text-gray-700 dark:text-gray-400  rounded-lg ">
+      <div className="from-[#b09da8] via-[#ebfffe] to-white bg-opacity-10">
         <div className=" px-4 py-10 md:py-20">
-          <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
+          <p className="h-0.5 w-full bg-white dark:bg-gray-700" />
           <div className="max-w-6xl mx-auto flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
             <div>
               <p>&copy; Nvvvrro. All Rights Reserved.</p>
@@ -14,8 +15,8 @@ export const Footer: FC = () => {
 
             <div className="flex flex-col md:flex-row">
               <p>Developed by</p>
-              <div className="flex flex-col md:flex-row transform hover:scale-105 hover:-rotate-3 transition duration-300">
-                <div className="hover:bg-indigo-500 rounded-md px-2  hover:text-gray-50">
+              <div className="flex flex-col md:flex-row transform hover:scale-105 transition duration-300">
+                <div className="rounded-md px-1 hover:bg-gradient-to-r hover:from-[#b09da8] hover:to-indigo-500  hover:text-gray-50">
                   <Link href={user.socialLinks.github}>Marcelo Navarro</Link>
                 </div>
               </div>
