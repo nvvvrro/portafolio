eb99c682e855be09413ff4449d904efb4c34098d
Marcelo Navarro <84986697+nvvvrro@users.noreply.github.com>
2022-07-04 23:57:12 -0400

    fix: size screen of footer to rrss

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,14 +6,10 @@ export const Footer: FC = () => {
   return (
     <footer className="text-gray-700 dark:text-gray-400  rounded-lg ">
       <div className="from-[#b09da8] via-[#ebfffe] to-white bg-opacity-10">
-        <div className=" px-4 py-10 pt-0 md:py-20">
+        <div className=" px-4 py-4 pt-0 sm:py-20">
           <p className="h-0.5 w-full bg-white dark:bg-gray-700" />
           <div className="max-w-6xl mx-auto flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
-            <div>
-              <p>&copy; Nvvvrro. All Rights Reserved.</p>
-            </div>
-
-            <div className="flex flex-col md:flex-row">
+            <div className="hidden sm:flex flex-col md:flex-row">
               <p>Developed by</p>
               <div className="flex flex-col md:flex-row transform hover:scale-105 transition duration-300">
                 <div className="rounded-md px-1 hover:bg-gradient-to-r hover:from-[#b09da8] hover:to-indigo-500  hover:text-gray-50">
